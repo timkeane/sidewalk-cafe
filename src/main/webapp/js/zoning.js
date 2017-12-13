3352b38509f27225a6c94d552ebcee92e6c617ef
timkeane <tkeane@doitt.nyc.gov>
2017-12-13 14:33:49 -0500

    cleanup

--- src/main/webapp/js/zoning.js
+++ src/main/webapp/js/zoning.js
@@ -1,6 +1,6 @@
 nyc.zoning = {
-  ESRI_URL: 'http://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/nysidewalkcafe/FeatureServer/0',
-  ESRI_FMT: new ol.format.EsriJSON(),
+  URL: 'http://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/nysidewalkcafe/FeatureServer/0/query/?f=json&returnGeometry=true&spatialRel=esriSpatialRelIntersects&geometryType=esriGeometryEnvelope&inSR=102100&outFields=*&outSR=102100&geometry=',
+  format: new ol.format.EsriJSON(),
   source: function(){
     nyc.zoning.source = new ol.source.Vector({
       loader: nyc.zoning.loader,
@@ -16,19 +16,21 @@ nyc.zoning = {
     });
   },
   loader: function(extent, resolution, projection){
-    var url = nyc.zoning.ESRI_URL +
-      '/query/?f=json&returnGeometry=true&spatialRel=esriSpatialRelIntersects&geometry=' +
-      encodeURIComponent(
-        '{"xmin":' + extent[0] + ',"ymin":' + extent[1] +
-        ',"xmax":' + extent[2] + ',"ymax":' + extent[3] +
-        ',"spatialReference":{"wkid":102100}}'
-      ) +
-      '&geometryType=esriGeometryEnvelope&inSR=102100&outFields=*&outSR=102100';
-      $.ajax({url: url, dataType: 'jsonp', success: function(response){
+    var bbox = {
+      xmin: extent[0],
+      ymin: extent[1],
+      xmax: extent[2],
+      ymax: extent[3],
+      spatialReference:{wkid: 102100}
+    };
+    $.ajax({
+      url: nyc.zoning.URL + encodeURIComponent(JSON.stringify(bbox)),
+      dataType: 'jsonp',
+      success: function(response){
         if (response.error){
-          console.console(response.error.message, response.error.details.join('\n'));
+          console.console(response.error.message, response.error.details);
         }else{
-          var features = nyc.zoning.ESRI_FMT.readFeatures(response, {featureProjection: projection});
+          var features = nyc.zoning.format.readFeatures(response, {featureProjection: projection});
           if (features.length > 0){
             nyc.zoning.source.addFeatures(features);
           }
